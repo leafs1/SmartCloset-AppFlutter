84a3ef25be9b3b54d8190a3d87eb5b2ba44b4229
leafs1 <michael.adragna1@gmail.com>
2020-03-30 15:32:39 -0400

    Cards are in rows of 2 in a column and everything works so far.

--- lib/ClosetScreen.dart
+++ lib/ClosetScreen.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:smart_closet_flutter/DrawerScreen.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:image/image.dart' as img;
 
 class Closet extends StatefulWidget {
   @override
@@ -31,6 +32,64 @@ class _ClosetState extends State {
     });
   }
 
+  Widget card(path){
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
+      child: Column(
+        children: <Widget>[
+          Image.file(File(path)),
+          Text("Image")
+        ],
+      ),
+    );
+  }
+
+  Widget getRow(cardsList) {
+    if (cardsList.length == 1){
+      return Row(
+        children: <Widget>[
+          Expanded(
+            child: new Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: cardsList[0],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.white,
+            ),
+          )
+
+
+        ],
+      );
+    } else {
+      return Row(
+        children: <Widget>[
+          Expanded(
+            child: new Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: cardsList[0],
+            ),
+          ),
+          Expanded(
+            child: new Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: cardsList[1],
+            ),
+          ),
+        ],
+      );
+    }
+  }
+
+  String getPath(index) {
+    String fileWExtra = file[index].toString();
+    int startIdx = fileWExtra.indexOf("/data");
+    String path = fileWExtra.substring(startIdx, fileWExtra.length-1);
+    return path;
+  }
+
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text("My Closet"), backgroundColor: Colors.blueGrey),
@@ -42,20 +101,40 @@ class _ClosetState extends State {
                   Expanded(
                     child: ListView.separated(
                       itemBuilder: (BuildContext context, int index) {
-                          //print(file[0].toString());
+                          print("length = " + file.length.toString());
+                          //print(file.toString());
 
-                          String fileWExtra = file[index].toString();
-                          int startIdx = fileWExtra.indexOf("/data");
-
-                          String path = fileWExtra.substring(startIdx, fileWExtra.length-1);
                           //print(fileWExtra.substring(startIdx, fileWExtra.length-1));
                           
-                          return Image.file(File(path));
+                          //Image image = Image.file(File(path));
+                          int usedIndex = index * 2;
+
+                          print("index = " + usedIndex.toString());
+
+                          if (file.length % 2 == 1 && usedIndex == file.length-1) {
+                            String path1 = getPath(usedIndex);
+                            Card card1 = card(path1);
+                            List cards = [card1];
+                            print("second");
+                            return getRow(cards);
+
+                          } else {
+                            String path1 = getPath(usedIndex);
+                            Card card1 = card(path1);
+
+                            String path2 = getPath(usedIndex+1);
+                            Card card2 = card(path2);
+
+                            List cards = [card1,card2];
+                            print("first");
+                            return getRow(cards);
+                          }
+
                         }, 
                       separatorBuilder: (BuildContext context, int index) {
                         return Divider();
                       }, 
-                      itemCount: file.length),
+                      itemCount: (file.length/2).ceil()),
                   )
                 ],
               ),
